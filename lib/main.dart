c12ae29b34826eb70c801ead66ce31bfe215a279
Munibullah Shah <49975985+MunibullahShah@users.noreply.github.com>
2022-06-28 04:44:59 +0500

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pagination/APIModel/airline_passangers_entity.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -28,13 +33,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int current_page = 0;
+
+  List<AirlinePassangersData> passengers = [];
+
+  @override
+  void initState() {
+    getPassenger();
+  }
+
+  getPassenger() async {
+    final Uri uri = Uri.parse(
+        "https://api.instantwebtools.net/v1/passenger?page=$current_page&size=10#");
+    var response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var result = AirlinePassangersEntity.fromJson(decodedData);
+      passengers = result.data!;
+      current_page++;
+    }
+    setState(() {
+      print("Changed");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Pagination"),
       ),
-      body: Container(),
+      body: passengers.length == 0
+          ? Container()
+          : ListView.builder(
+              itemCount: passengers.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(passengers[index].name!),
+                );
+              },
+            ),
     );
   }
 }
